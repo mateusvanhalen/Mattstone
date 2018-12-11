95a73a8170f87aa3c11b766b7204d78c5e37d833
Matthew Hall <mateusvanhalen@gmail.com>
2018-12-11 13:40:46 -0600

    first with ignore

---
+++ Mattstone/Models/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mattstone.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Family Role")]
+        public string FamilyRole { get; set; }
+
+        [Required]
+        [Display(Name = "Family")]
+        public Family Family { get; set; }
+
+        [Required]
+        public int FamilyId { get; set; }
+        
+    }
+}
---
+++ Mattstone/Models/Family.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mattstone.Models
+{
+    public class Family
+    {
+        [Key]
+        [Required]
+        public int FamilyId { get; set; }
+        [Required]
+        public string FamilyName { get; set; }
+    }
+}
