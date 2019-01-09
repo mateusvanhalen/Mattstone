1c5d199592ad74c8ea89cbb4cd0b4807a58d2e8d
Matthew Hall <mateusvanhalen@gmail.com>
2019-01-08 18:18:19 -0600

    styles

--- Mattstone/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Mattstone/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,7 +44,7 @@ namespace Mattstone.Areas.Identity.Pages.Account
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
-
+            
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
